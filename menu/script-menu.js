0b6a1dd8abf6fe1a5e3eebaebc7689e86301cfed
chikchicly <chikchicly@gmail.com>
2025-01-29 15:12:28 +0700

    menu

--- menu/script-menu.js
+++ menu/script-menu.js
@@ -2,34 +2,64 @@ window.addEventListener('scroll', menU);
 window.addEventListener('DOMContentLoaded',menU);
 function menU(){
   if(document.querySelector('.burg')){
-    // console.log('burger Yest');
-    let count = +0;
-    document.querySelector('.burg').onclick=function(){
-            count +=1;
-            //-----------------
-            //------open-close---
-            //------------------
+    var count = +0;
+    document.querySelector('.burg').addEventListener('click',opMenu);
+    
+    function opMenu(){
+            count++;
+            // console.log(count);
             document.getElementsByTagName('body')[0].style.cssText=`overflow:auto`;
-            //-------------------
-        if(count<=1){
+        if(count==1){
             document.querySelector("#offmenu").classList.add("openmenu");
-                            //---------------------------------
-                            document.getElementsByTagName('body')[0].style.cssText=`overflow:hidden`;
-                            //--------------------------------
+            document.getElementsByTagName('body')[0].style.cssText=`overflow:hidden`;
+          document.querySelector('#offmenu').style.cssText=`
+            width:0;
+            transition: ease 2s;
+            `;
+          setTimeout(()=>{
+            document.querySelector('#offmenu').style.cssText=`
+            width:100%;
+            opacity:1;
+            font-size:20px;
+            transition: ease 2s;
+            `;
+          },100);
         }else{
-            document.querySelector("#offmenu").classList.remove('openmenu');
+              // console.log(count + " else !!!!");
+              
+          document.querySelector('#offmenu').style.cssText=`
+            width:0%;
+            opacity:0;
+            font-size:0px;
+            transition: ease 2s;
+            `;
+            setTimeout(()=>{
+              closeMenuoK();
+              document.querySelector("#offmenu").classList.remove('openmenu');
+            },1000);
             count =0;
-            console.log("Menu close!");
+            // console.log("Menu close!");
         }
     }
   }else{
     // console.log('burgeraNet');
+    console.log(count);
   }
 }
 function CloseBurger(){
   document.querySelector("#offmenu").classList.remove('openmenu');
   document.getElementsByTagName('body')[0].style.cssText=`overflow:auto`;
   count =0;
+}
+function closeMenuoK(){
+  document.querySelector('#offmenu').style.cssText=`
+    width:0;
+    transition: ease 2s;
+  `;
+  setTimeout(()=>{
+    document.querySelector("#offmenu").classList.remove('openmenu');
+    count=0;
+},300);
 }
 // if(document.querySelector('.burg')){
 //   document.querySelector('.burg').addEventListener('dblclick', function(){
