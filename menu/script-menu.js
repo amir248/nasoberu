c9b17250777b5f1c069c8316210fc67f947c9ae2
chikchicly <chikchicly@gmail.com>
2025-01-29 16:40:08 +0700

    oooO

--- menu/script-menu.js
+++ menu/script-menu.js
@@ -4,14 +4,14 @@ function menU(){
   if(document.querySelector('.burg')){
     var count = +0;
     document.querySelector('.burg').addEventListener('click',opMenu);
+    document.querySelector('#offmenu').addEventListener('click',opMenu);
     
     function opMenu(){
-            count++;
-            // console.log(count);
+            count +=1;
             document.getElementsByTagName('body')[0].style.cssText=`overflow:auto`;
-        if(count==1){
+        if(count<=1){
             document.querySelector("#offmenu").classList.add("openmenu");
-            document.getElementsByTagName('body')[0].style.cssText=`overflow:hidden`;
+                            document.getElementsByTagName('body')[0].style.cssText=`overflow:hidden`;
           document.querySelector('#offmenu').style.cssText=`
             width:0;
             transition: ease 2s;
@@ -19,60 +19,22 @@ function menU(){
           setTimeout(()=>{
             document.querySelector('#offmenu').style.cssText=`
             width:100%;
-            opacity:1;
-            font-size:20px;
             transition: ease 2s;
             `;
           },100);
         }else{
-              // console.log(count + " else !!!!");
-              
           document.querySelector('#offmenu').style.cssText=`
-            width:0%;
-            opacity:0;
-            font-size:0px;
+            width:0;
             transition: ease 2s;
             `;
-            // setTimeout(()=>{
-            //   closeMenuoK();
-            //   document.querySelector("#offmenu").classList.remove('openmenu');
-            // },1000);
+            setTimeout(()=>{
+              document.querySelector("#offmenu").classList.remove('openmenu');
+            },300);
             count =0;
             // console.log("Menu close!");
         }
     }
   }else{
     // console.log('burgeraNet');
-    console.log(count);
   }
 }
-function CloseBurger(){
-  document.querySelector('#offmenu').style.cssText=`
-            width:00%;
-            opacity:0;
-            font-size:0px;
-            transition: ease 2s;
-            `;
-  document.querySelector("#offmenu").classList.remove('openmenu');
-  document.getElementsByTagName('body')[0].style.cssText=`overflow:auto`;
-  count=0;
-}
-function closeMenuoK(){
-  document.querySelector('#offmenu').style.cssText=`
-    width:0;
-    transition: ease 2s;
-  `;
-  setTimeout(()=>{
-    document.querySelector("#offmenu").classList.remove('openmenu');
-    count=0;
-},300);
-}
-// if(document.querySelector('.burg')){
-//   document.querySelector('.burg').addEventListener('dblclick', function(){
-//     window.location.href = 'https://nasobe.ru';
-//   });
-//
-// }else{
-//   console.log('Бургера нет!');
-// }
-document.querySelector('#offmenu').addEventListener('click',opMenu);
